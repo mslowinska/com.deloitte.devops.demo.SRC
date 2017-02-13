05aa5748eb4086137e7cf47aa13d754c2bb53eb2
'mslowinska' <mslowinska@deloitte.co.uk>
2017-02-13 14:47:59 +0000

    Additional validation for First name added

--- src/main/java/com/deloitte/devops/validator/UserValidator.java
+++ src/main/java/com/deloitte/devops/validator/UserValidator.java
@@ -3,7 +3,8 @@ package com.deloitte.devops.validator;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import com.deloitte.devops.model.User;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class UserValidator implements Validator {
 
 	@Override
@@ -20,6 +21,12 @@ public class UserValidator implements Validator {
 		if(user.getFirstName().isEmpty()) {
 			errors.rejectValue("firstName", "NotEmpty.firstName");	
 		}
+		
+		if(user.getFirstName().length()<2 && user.getFirstName().length()>0) {
+			errors.rejectValue("firstName", "Min2.firstName");
+		}
+		
+/*		if(user.getFirstName().)*/
 		
 		if(user.getLastName().isEmpty()) {
 			errors.rejectValue("lastName", "NotEmpty.lastName");	
@@ -63,3 +70,30 @@ public class UserValidator implements Validator {
 		
 	}
 }
+
+
+
+/*private Pattern pattern;
+private Matcher matcher;
+
+private static final String EMAIL_PATTERN =
+	"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+	+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+public EmailValidator() {
+	pattern = Pattern.compile(EMAIL_PATTERN);
+}
+
+*//**
+ * Validate hex with regular expression
+ *
+ * @param hex
+ *            hex for validation
+ * @return true valid hex, false invalid hex
+ *//*
+public boolean validate(final String hex) {
+
+	matcher = pattern.matcher(hex);
+	return matcher.matches();
+
+}*/
