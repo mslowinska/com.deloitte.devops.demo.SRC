5eeb263c457bc614ed7e2982e7dc6cc19908c2e1
'mslowinska' <mslowinska@deloitte.co.uk>
2017-02-16 16:58:58 +0000

    One Unit Test fixed, negative still needs fixing

--- src/test/java/com/deloitte/devops/demoTest.java
+++ src/test/java/com/deloitte/devops/demoTest.java
@@ -32,53 +32,55 @@ public class demoTest {
 
     @Autowired
     private UserValidator validator;
-    private User user1;
-    private User user2;
+    private User user_test1;
+    private User user_test2;
     public Errors errors1;
     public Errors errors2;
 
     @Before
     public void setUp() {
-        user1 = new User();
-        user1.setFirstName("John");
-        user1.setLastName("Doe");
-        user1.setEmail("email");
-        user1.setAddress1("address1");
-        user1.setAddress2("address2");
-        user1.setPostcode("SE113UU");
-        user1.setCity("city");
-        user1.setNameCard("John Doe");
-        user1.setNumberCard("1234123412341234");
-        user1.setExpiryDate("12/12");
-        user1.setSecurityCard("123");
+        user_test1 = new User();
+        user_test1.setFirstName("John");
+        user_test1.setLastName("Doe");
+        user_test1.setEmail("jdoe@gmail.com");
+        user_test1.setPhone("07777777777");
+        user_test1.setAddress1("23 XX Street");
+        user_test1.setAddress2("XXX");
+        user_test1.setPostcode("SE113UU");
+        user_test1.setCity("London");
+        user_test1.setNameCard("John Doe");
+        user_test1.setNumberCard("1234123412341234");
+        user_test1.setExpiryDate("12/12");
+        user_test1.setSecurityCard("123");
 
-        errors1 = new BeanPropertyBindingResult(user1, "user1");
+        errors1 = new BeanPropertyBindingResult(user_test1, "user_test1");
         
-        user2 = new User();
-        user2.setFirstName("");
-        user2.setLastName("Doe");
-        user2.setEmail("email");
-        user2.setAddress1("address1");
-        user2.setAddress2("address2");
-        user2.setPostcode("SE113UU");
-        user2.setCity("city");
-        user2.setNameCard("John Doe");
-        user2.setNumberCard("1234123");
-        user2.setExpiryDate("12/12");
-        user2.setSecurityCard("123");
+        user_test2 = new User();
+        user_test2.setFirstName("");
+        user_test2.setLastName("Doe");
+        user_test2.setEmail("email");
+        user_test1.setPhone("0");
+        user_test2.setAddress1("address1");
+        user_test2.setAddress2("address2");
+        user_test2.setPostcode("SE113UU");
+        user_test2.setCity("city");
+        user_test2.setNameCard("John Doe");
+        user_test2.setNumberCard("1234123");
+        user_test2.setExpiryDate("12/12");
+        user_test2.setSecurityCard("123");
 
-        errors2 = new BeanPropertyBindingResult(user2, "user2");
+        errors2 = new BeanPropertyBindingResult(user_test2, "user_test2");
     }
 
     @Test
     public void testValidate1Positive() {
-        validator.validate(user1, errors1);
+        validator.validate(user_test1, errors1);
         assertFalse(errors1.hasErrors());
     }
     
     @Test
     public void testValidate2Negative() {
-        validator.validate(user2, errors2);
+        validator.validate(user_test2, errors2);
         assertTrue(errors2.hasErrors());
     }
 }
