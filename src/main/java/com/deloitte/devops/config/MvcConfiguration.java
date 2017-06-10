969e6cd9c706c135e0b6ec74dfeeb5e474d2ca0e
'mslowinska' <mslowinska@deloitte.co.uk>
2017-06-10 14:03:37 +0100

    SonarQube - unused imports deleted

--- src/main/java/com/deloitte/devops/config/MvcConfiguration.java
+++ src/main/java/com/deloitte/devops/config/MvcConfiguration.java
@@ -2,17 +2,12 @@ package com.deloitte.devops.config;
 
 import java.util.Properties;
 
-import javax.annotation.processing.SupportedAnnotationTypes;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
